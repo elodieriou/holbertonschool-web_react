588f6df310fc0b656f4a0933f0708bfb4b8586ae
Elodie Riou <elodie.riou@hotmail.com>
2023-04-27 15:41:32 +0200

    FEATURE: implement brand convention and nominal typing

---
+++ 0x00-TypeScript/task_5/js/main.ts
@@ -0,0 +1,19 @@
+import {appendSuffixesIfMatch} from "ts-loader/dist/utils";
+
+interface MajorCredits {
+    credits: number;
+    brand: 'Hello world';
+}
+
+interface MinorCredits {
+    credits: number;
+    brand: 'Holberton school';
+}
+
+function sumMajorCredits(subject1: number, subject2: number): number {
+    return subject1 + subject2;
+}
+
+function sumMinorCredits(subject1: number, subject2: number): number {
+    return
+}
