261ebf66ea9fd475cfb8eb506a86d0a179027e52
Elodie Riou <elodie.riou@hotmail.com>
2023-05-02 15:11:30 +0200

    FIX: issue

--- 0x00-TypeScript/task_5/js/main.ts
+++ 0x00-TypeScript/task_5/js/main.ts
@@ -1,23 +1,23 @@
 interface MajorCredits {
     credits: number;
-    __brand: 'Hello world';
+    __brand: 'Major';
 }
 
 interface MinorCredits {
     credits: number;
-    __brand: 'Holberton school';
+    __brand: 'Minor';
 }
 
 function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
     return {
         credits: subject1.credits + subject2.credits,
-        __brand: 'Hello world',
+        __brand: 'Major',
     };
 }
 
 function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
     return {
         credits: subject1.credits + subject2.credits,
-        __brand: 'Holberton school',
+        __brand: 'Minor',
     };
 }
