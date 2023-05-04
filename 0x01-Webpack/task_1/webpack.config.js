d17ee1a07c435cab5f061847fe78073f7695693e
Elodie Riou <elodie.riou@hotmail.com>
2023-05-04 09:00:47 +0200

    FIX: webpackconfig

--- 0x01-Webpack/task_1/webpack.config.js
+++ 0x01-Webpack/task_1/webpack.config.js
@@ -1,12 +1,12 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'production',
     entry: {
-      main: './js/dashboard_main.js',
+      main: path.resolve(__dirname, './js/dashboard_main.js')
     },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'public')
     },
+    mode: 'production',
 };
