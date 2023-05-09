b7e1863d7443a0ac9d2af3d2d65421bd4dcb20e6
Elodie Riou <elodie.riou@hotmail.com>
2023-05-09 19:05:46 +0200

    FEATURE: Create a project using Webpack

---
+++ 0x02-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, "./src/index.js")
+    },
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, './dist')
+    },
+    devServer: {
+        contentBase: './dist',
+        hot: true,
+        port: 3000,
+        open: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './dist/index.html',
+            title: 'Holberton Dashboard',
+        }),
+        new CleanWebpackPlugin()
+    ],
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpe?g|gif)$/i,
+                type: 'asset/resource',
+                loader: 'image-webpack-loader',
+            },
+        ],
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
+};
