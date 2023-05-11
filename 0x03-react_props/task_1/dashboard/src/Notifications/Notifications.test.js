a1d015a12a123073c17b7dbd463d2e85374878dc
Elodie Riou <elodie.riou@hotmail.com>
2023-05-11 11:31:52 +0200

    FEATURE: 1. Write the tests for each component

---
+++ 0x03-react_props/task_1/dashboard/src/Notifications/Notifications.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Notifications from "./Notifications";
+
+describe('Notifications component tests', () => {
+
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<Notifications />);
+    });
+
+    it('renders without crashing', () => {
+        expect(wrapper.exists()).toBe(true);
+    });
+
+    it('renders three list items', () => {
+        const list = wrapper.find('ul');
+        const listItems = list.find('li');
+        expect(listItems).toHaveLength(3);
+    });
+
+    it('renders the text', () => {
+        const textElement = wrapper.find('p');
+        expect(textElement.text()).toBe('Here is the list of notifications');
+    });
+});
