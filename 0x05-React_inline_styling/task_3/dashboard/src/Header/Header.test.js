84154c6e26bbe2b7bb1a7ae6bd9d7a3999ef6f8e
Elodie Riou <elodie.riou@hotmail.com>
2023-05-24 17:10:50 +0200

    FEATURE: 3. Responsive design

---
+++ 0x05-React_inline_styling/task_3/dashboard/src/Header/Header.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Header from './Header';
+import { StyleSheetTestUtils } from 'aphrodite';
+
+
+describe('Header component tests', () => {
+
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<Header />);
+    });
+
+    it('renders Header without crashing', () => {
+        expect(wrapper.exists()).toBe(true);
+    });
+
+    it('renders a div with the class App-header', () => {
+        const appHeader = wrapper.find('[className^="header"]');
+        expect(appHeader).toHaveLength(1);
+    });
+
+    it('renders a tag img', () => {
+        const appHeader = wrapper.find('img');
+        expect(appHeader).toHaveLength(1);
+    });
+
+    it('renders a tag h1', () => {
+        const appHeader = wrapper.find('h1');
+        expect(appHeader).toHaveLength(1);
+    });
+});
