4a5ec007fa1abc7fc94de2db248567b1b439c9f7
Elodie Riou <elodie.riou@hotmail.com>
2023-06-12 16:08:15 +0200

    FEATURE: 8. Lazy Seq

---
+++ 0x07-React_Immutable/8-seq.js
@@ -0,0 +1,21 @@
+import { Seq, Map } from 'immutable';
+
+const printBestStudents = (object) => {
+    const sequence = Seq(object);
+    const getStudents = sequence.filter((student) => student.score > 70);
+    const students = getStudents.toJS();
+
+    const _toUpperCase = (string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    Object.keys(students).map((student) => {
+        students[student].firstName = _toUpperCase(students[student].firstName);
+        students[student].lastName = _toUpperCase(students[student].lastName);
+        return students[student];
+    });
+
+    console.log(students);
+}
+
+export default printBestStudents;
