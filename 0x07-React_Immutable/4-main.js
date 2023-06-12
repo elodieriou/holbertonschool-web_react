4bad4fb106c6700321943124914fbfe950cc3c26
Elodie Riou <elodie.riou@hotmail.com>
2023-06-12 11:32:57 +0200

    FIX: 4-main

--- 0x07-React_Immutable/4-main.js
+++ 0x07-React_Immutable/4-main.js
@@ -1,6 +1,4 @@
 import { map, map2 } from './4-mutations';
 
-console.log(map.get('2'));
-console.log(map2.get(2));
-console.log(map.get('4'));
-console.log(map2.get(4));
+console.log(map.toJS());
+console.log(map2.toJS());
