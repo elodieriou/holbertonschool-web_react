b91074ab0df84b7ab4dcbf5dc50c960b5a9930a6
Elodie Riou <elodie.riou@hotmail.com>
2023-06-14 19:21:45 +0200

    FEATURE: 5. Create actions for the notification list

---
+++ 0x08_react_redux_action_creator_normalizr/task_5/dashboard/src/actions/uiActionCreators.test.js
@@ -0,0 +1,39 @@
+import {
+    login,
+    logout,
+    displayNotificationDrawer,
+    hideNotificationDrawer
+} from './uiActionCreators';
+import {
+    LOGIN,
+    LOGOUT,
+    DISPLAY_NOTIFICATION_DRAWER,
+    HIDE_NOTIFICATION_DRAWER
+} from "./uiActionTypes";
+
+describe('ui actions creators tests', () => {
+    it('test action created on login', () => {
+        const email = '3685@holbertonstudents.com';
+        const password = 'azerty';
+        const actionLogin = {
+            type: LOGIN,
+            user: { email, password }
+        };
+        expect(login(email, password)).toEqual(actionLogin);
+    });
+
+    it('test action created on logout', () => {
+        const actionLogout = { type: LOGOUT };
+        expect(logout()).toEqual(actionLogout);
+    });
+
+    it('test action created on display notification drawer', () => {
+        const actionDisplayNotification = { type: DISPLAY_NOTIFICATION_DRAWER };
+        expect(displayNotificationDrawer()).toEqual(actionDisplayNotification);
+    });
+
+    it('test action created on hide notification drawer', () => {
+        const actionHideNotification = { type: HIDE_NOTIFICATION_DRAWER };
+        expect(hideNotificationDrawer()).toEqual(actionHideNotification);
+    });
+});
