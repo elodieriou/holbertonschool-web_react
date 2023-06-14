16b025546fac432867d6749a552d21ce6a8af2fe
Elodie Riou <elodie.riou@hotmail.com>
2023-06-14 17:06:24 +0200

    FEATURE: 2. Filter a normalized Schema

--- 0x08_react_redux_action_creator_normalizr/task_2/dashboard/src/schema/notifications.js
+++ 0x08_react_redux_action_creator_normalizr/task_2/dashboard/src/schema/notifications.js
@@ -2,6 +2,18 @@ import * as notificationsData from '../../notifications.json';
 import { normalize, schema } from 'normalizr';
 import notifications from "../Notifications/Notifications";
 
+const user = new schema.Entity('users');
+
+const message = new schema.Entity('messages', {}, { idAttribute: 'guid' });
+
+const notification = new schema.Entity('notifications',
+    {
+    author: user,
+    context: message,
+});
+
+export const normalizedData = normalize(notificationsData.default, [notification]);
+
 export const getAllNotificationsByUser = (userId) => {
     const notifications = normalizedData.entities.notifications;
     const messages = normalizedData.entities.messages;
@@ -14,15 +26,3 @@ export const getAllNotificationsByUser = (userId) => {
     }
     return allNotificationsByUser;
 }
-
-const user = new schema.Entity('users');
-
-const message = new schema.Entity('messages', {}, { idAttribute: 'guid' });
-
-const notification = new schema.Entity('notifications',
-    {
-    author: user,
-    context: message,
-});
-
-export const normalizedData = normalize(notificationsData.default, [notification]);
