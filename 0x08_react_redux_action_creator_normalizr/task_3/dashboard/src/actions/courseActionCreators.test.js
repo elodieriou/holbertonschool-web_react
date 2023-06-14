ac0b2f274e334de3d58eba2d186e3ab9732cccab
Elodie Riou <elodie.riou@hotmail.com>
2023-06-14 19:22:17 +0200

    FIX: code

--- 0x08_react_redux_action_creator_normalizr/task_3/dashboard/src/actions/courseActionCreators.test.js
+++ 0x08_react_redux_action_creator_normalizr/task_3/dashboard/src/actions/courseActionCreators.test.js
@@ -1,13 +1,14 @@
 import { selectCourse, unSelectCourse } from './courseActionCreators';
+import { SELECT_COURSE, UNSELECT_COURSE } from './courseActionTypes';
 
-describe('Actions creators tests', () => {
+describe('Courses actions creators tests', () => {
     it('test action created on select course', () => {
-        const actionSelect = { type: 'SELECT_COURSE', index: 1 };
+        const actionSelect = { type: SELECT_COURSE, index: 1 };
         expect(selectCourse(1)).toEqual(actionSelect);
     });
 
     it('test action created on unselect course', () => {
-        const actionUnSelect = { type: 'UNSELECT_COURSE', index: 1 };
+        const actionUnSelect = { type: UNSELECT_COURSE, index: 1 };
         expect(unSelectCourse(1)).toEqual(actionUnSelect);
     });
 });
