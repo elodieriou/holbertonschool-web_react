de0e8868e05a9973e7a8db699fd417ec2b7cb485
Elodie Riou <elodie.riou@hotmail.com>
2023-06-27 16:43:21 +0200

    FEATURE: Add tests

--- 0x0A-react_redux_connectors_and_providers/task_7/dashboard/src/CourseList/CourseList.test.js
+++ 0x0A-react_redux_connectors_and_providers/task_7/dashboard/src/CourseList/CourseList.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import CourseList from './CourseList';
+import { CourseList } from './CourseList';
+import { fromJS } from 'immutable';
+import {fetchCourses} from "../actions/courseActionCreators";
 
 describe('CourseList component tests', () => {
 
@@ -12,7 +14,7 @@ describe('CourseList component tests', () => {
                 {id: 1, name: "ES6", credit: 60},
                 {id: 2, name: "Webpack", credit: 20},
                 {id: 3, name: "React", credit: 40},
-            ]
+            ];
             wrapper = shallow(<CourseList listCourses={listCourses}/>);
         });
 
@@ -41,6 +43,53 @@ describe('CourseList component tests', () => {
             const row = wrapper.find('CourseListRow');
             expect(row).toHaveLength(3);
             expect(row.at(2).prop('textFirstCell')).toEqual('No course available yet');
+        });
+    });
+
+    describe('Actions dispatched', () => {
+
+        let listCourses;
+
+        beforeEach(() => {
+            listCourses = [
+                {id: 1, name: "ES6", credit: 60},
+                {id: 2, name: "Webpack", credit: 20},
+                {id: 3, name: "React", credit: 40},
+            ];
+
+        });
+
+        afterEach(() => {
+            jest.restoreAllMocks();
+        });
+
+        it('check if fetchCourses action is dispatched when the component is mounted', () => {
+            const fetchCourses = jest.fn();
+            shallow(<CourseList listCourses={listCourses} fetchCourses={fetchCourses}/>);
+            expect(fetchCourses).toHaveBeenCalledTimes(1);
+        });
+
+        it('check if selectCourse action is dispatched when the onChangeRow function is called', () => {
+            const selectCourse = jest.fn();
+            const wrapper = shallow(<CourseList
+                listCourses={listCourses}
+                selectCourse={selectCourse}
+                fetchCourses={fetchCourses}
+            />);
+            wrapper.instance().onChangeRow(1, true);
+            expect(selectCourse).toHaveBeenCalledTimes(1);
+
+        });
+
+        it('check if unSelectCourse action is dispatched when the onChangeRow function is called', () => {
+            const unSelectCourse = jest.fn();
+            const wrapper = shallow(<CourseList
+                listCourses={listCourses}
+                unSelectCourse={unSelectCourse}
+                fetchCourses={fetchCourses}
+            />);
+            wrapper.instance().onChangeRow(1, false);
+            expect(unSelectCourse).toHaveBeenCalledTimes(1);
         });
     });
 });
